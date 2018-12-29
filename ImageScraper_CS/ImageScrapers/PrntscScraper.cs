9c5d8f5f54130f99f8bf0d78f6541e4e88c8dc33
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-29 18:22:18 +0200

    feat: add scraper for prnt.sc site

---
+++ ImageScraper_CS/ImageScrapers/PrntscScraper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageScrapers
+{
+    public class PrntscScraper : IScraper {
+
+        public void StartDownloading() {
+            throw new NotImplementedException();
+        }
+    }
+}
