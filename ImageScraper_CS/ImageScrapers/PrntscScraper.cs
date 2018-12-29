f70fcaf5a50054e5a4f498973fdefc4f240b4c58
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-29 18:58:15 +0200

    feat(PrntscScraper): add html parser

--- ImageScraper_CS/ImageScrapers/PrntscScraper.cs
+++ ImageScraper_CS/ImageScrapers/PrntscScraper.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ImageScrapers
 {
     public class PrntscScraper : IScraper {
+        private const string linkPattern = "https://prnt.sc/";
         private List<char> _alphabet = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
                                                         'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                                                         'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
@@ -15,7 +18,15 @@ namespace ImageScrapers
 
         public void StartDownloading() {
             string image;
-            image = GenerateImageName();
+            string html;
+            string link;
+
+            using (WebClient webClient = new WebClient()) {
+                image = GenerateImageName();
+                webClient.Headers.Add("User-Agent: Other");
+                html = webClient.DownloadString($"{linkPattern}{image}");
+                link = ParseHtml(html);
+            }
 
             throw new NotImplementedException();
         }
@@ -26,6 +37,12 @@ namespace ImageScrapers
             for (int i = 0; i < 6; i++)
                 image += Alphabet[random.Next(Alphabet.Count)];
             return image;
+        }
+
+        private string ParseHtml(string html) {
+            string meta = Regex.Match(html, "og:image.+?/>").Value;
+            string content = Regex.Match(meta, "content=.+?/>").Value;
+            return Regex.Match(content, "\".+?\"").Value.Trim(new char[] { '"' });
         }
     }
 }
