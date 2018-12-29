e5aa434cc487c76712011e4c2f7b1ab4a06d4036
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-29 19:37:37 +0200

    feat: add ScraperEventArgs

---
+++ ImageScraper_CS/ImageScrapers/ScraperEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageScrapers
+{
+    public class ScraperEventArgs : EventArgs {
+        public string Message { get; }
+        public string Image { get; }
+        public ScraperEventArgs(string message, string image) {
+            Message = message;
+            Image = image;
+        }
+    }
+}
