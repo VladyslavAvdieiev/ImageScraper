cac39d8dfb7b65cb5813cd692a72d60e5f410081
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-30 00:14:31 +0200

    feat: add implementation of console user interface

--- ImageScraper_CS/UI.ConsoleApp/Program.cs
+++ ImageScraper_CS/UI.ConsoleApp/Program.cs
@@ -7,10 +7,19 @@ using System.Threading.Tasks;
 
 namespace UI.ConsoleApp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+       
+        static void Main(string[] args) {
+            PrntscScraper scraper = new PrntscScraper();
+            scraper.OnStarted += (sender, e) => Console.WriteLine(e.Message);
+            scraper.OnImageDownloaded += (sender, e) => Console.WriteLine(e.Message);
+            scraper.OnErrorOccurred += (sender, e) => Console.WriteLine(e.Message);
+
+            Task.Run(() => scraper.StartDownloading());
+            Task.Run(() => scraper.StartDownloading());
+            Task.Run(() => scraper.StartDownloading());
+
+            Console.ReadLine();
         }
     }
 }
