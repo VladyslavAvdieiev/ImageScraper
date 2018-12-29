879239121693d261b28ac4631bd126d501f48c8d
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-29 18:36:43 +0200

    feat(PrntscScraper): add alphabet

--- ImageScraper_CS/ImageScrapers/PrntscScraper.cs
+++ ImageScraper_CS/ImageScrapers/PrntscScraper.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace ImageScrapers
 {
     public class PrntscScraper : IScraper {
+        private List<char> _alphabet = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+                                                        'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                                        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public IList<char> Alphabet { get => _alphabet.AsReadOnly(); }
 
         public void StartDownloading() {
             throw new NotImplementedException();
